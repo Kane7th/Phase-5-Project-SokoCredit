857e169d2028d852595ffbe6ea5d4b5de8c60e3f
Kane Kabena <onekaneldn@gmail.com>
2025-07-16 10:57:31 +0300

    flask database initialised

---
+++ backend/app/models/user.py
@@ -0,0 +1,14 @@
+from app.extensions import db
+from passlib.hash import pbkdf2_sha256
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), default='agent')  # 'admin' or 'agent'
+
+    def set_password(self, password):
+        self.password_hash = pbkdf2_sha256.hash(password)
+
+    def check_password(self, password):
+        return pbkdf2_sha256.verify(password, self.password_hash)
