6d2c576c04751c7249afc24bb37d071a66a9f221
shirocodes <ishirapt@gmail.com>
2025-07-17 19:56:23 +0300

    refactor(models): Implemented serializer rules to control depth.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,8 @@
 from app.extensions import db
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy_serializer import SerializerMixin 
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -11,8 +12,12 @@ class User(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), default='Mama Mboga', nullable=False)  # 'admin', 'mama mboga' or 'lender'
 
+    serialize_rules = ('id', 'username', 'role')
+    
     def set_password(self, password):
         self.password_hash = pbkdf2_sha256.hash(password)
 
     def check_password(self, password):
         return pbkdf2_sha256.verify(password, self.password_hash)
+    
+    
