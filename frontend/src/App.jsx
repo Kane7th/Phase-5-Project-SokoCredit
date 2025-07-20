b81fea7437db39564d495acef5009bb181161284
MaiyoDenis <denismaiyo14@gmail.com>
2025-07-20 16:31:39 +0300

    feat(common): implement Header component
    - Add user dropdown menu with profile/logout options
    - Include notification badge and dropdown
    - Implement mobile-responsive hamburger menu
    - Connect to App.js layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import LandingPage from './components/landing/LandingPage'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
-// Removed Dashboard import since it will not be used
+import DashboardRouter from './components/dashboard/Dashboard'
+
 import ProtectedRoute from './components/auth/ProtectedRoute'
 import Layout from './components/common/Layout'
 
@@ -22,8 +23,8 @@ function App() {
             <Layout />
           </ProtectedRoute>
         }>
-          {/* Muted Dashboard: replaced with empty fragment */}
-          <Route index element={null} />
+          {/* Unmuted Dashboard */}
+          <Route index element={<DashboardRouter />} />
         </Route>
         
         {/* Redirect to landing for any unmatched routes */}
