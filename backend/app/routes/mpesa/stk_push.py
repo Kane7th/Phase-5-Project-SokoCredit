0d394f275802fe7020a0a42edc6a6d8a018f4e5b
shirocodes <ishirapt@gmail.com>
2025-07-30 12:11:46 +0300

    fix(mpesa STK): Encoded consumer key and secret.

--- backend/app/routes/mpesa/stk_push.py
+++ backend/app/routes/mpesa/stk_push.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from flask import current_app
 from .client import get_access_token
 
-def send_stk_push(phone_number: str, amount: int, account_ref='TestSoko', description='Loan payments'):
+def send_stk_push(phone_number: str, amount: int, account_ref='MySoko', description='Loan payments'):
     """
     Helper func. handling STK Push request logic, later will be 
     imported into views.py.
@@ -22,8 +22,15 @@ def send_stk_push(phone_number: str, amount: int, account_ref='TestSoko', descri
     data_to_encode = business_shortcode + passkey + timestamp
     encoded_password = base64.b64encode(data_to_encode.encode()).decode()
     
+    url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+     
+    print("Token acquired at:", datetime.now().isoformat())    
+    print(f'access_token: {access_token}')
+    print(f'shortcode: {business_shortcode}')
+    print(f'passkey: {passkey}')
     print(f"Password: {encoded_password}")
     print(f"Timestamp: {timestamp}")
+ 
     # build request body
     payload = {
         'BusinessShortCode': business_shortcode,
@@ -43,12 +50,16 @@ def send_stk_push(phone_number: str, amount: int, account_ref='TestSoko', descri
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'
     }
+    # print("Payload being sent:", payload)
+    # print("Headers being sent:", headers)
     
-    response = requests.post(
-        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
-        json=payload,
-        headers=headers
-    )
-    return response.json()
+    try:
+        response = requests.post(
+            url, json=payload, headers=headers
+        )
+        print(response.json())
+        return response.json()
+    except Exception as e:
+        print("Error on STK-PUSH:", str(e))
 
 
