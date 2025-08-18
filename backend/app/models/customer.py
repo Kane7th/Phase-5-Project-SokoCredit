3d0b6d014bfe642b22d85be8114ce02dda60d783
MaiyoDenis <denismaiyo14@gmail.com>
2025-08-18 17:56:10 +0300

    fix: update customer model to ensure data integrity

--- backend/app/models/customer.py
+++ backend/app/models/customer.py
@@ -15,9 +15,6 @@ class Customer(db.Model, SerializerMixin):
     location = db.Column(db.String(100))
     documents = db.Column(db.JSON, default={}) 
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
-    lender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
-
-
     # The user (admin/lender) who created this customer manually
     created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
 
@@ -35,12 +32,6 @@ class Customer(db.Model, SerializerMixin):
         "User",
         foreign_keys=[customer_user_id],
         back_populates="customer_profile"
-    )
-
-    lender_id = db.Column(
-        db.Integer,
-        db.ForeignKey('users.id', name='fk_customers_lender_id_user'),
-        nullable=True
     )
 
     def __repr__(self):
